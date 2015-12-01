30786d4dee2825e9d15b882b2dc2de5977dab41e
BobbyCeps <BobbyCeps@gmail.com>
2015-12-01 16:39:37 +0100

    creation depot git pour la page du futsal

---
+++ nomequipe.js
@@ -0,0 +1,26 @@
+/*Fonction pour renseigner le titre de l'équipe*/
+function addTitre() {
+    var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq1").innerHTML = titre;
+}
+function addTitre2() {
+      var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq2").innerHTML = titre;
+}
+function addTitre3() {
+      var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq3").innerHTML = titre;
+}
+function addTitre4() {
+      var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq4").innerHTML = titre;
+}
+function addTitre5() {
+      var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq5").innerHTML = titre;
+}
+function addTitre6() {
+      var titre = prompt("Entrez le nom de l'équipe : ")
+      document.getElementById("titre_eq6").innerHTML = titre;
+}
+/* Seconde fonction pour remplir le nom des joueurs*/
